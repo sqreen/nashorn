0a84183ec3e46d55cfd2ff1dd25600ad9cce2781
Gustavo Lopes <mail@geleia.net>
2020-10-12 17:12:52 +0100

    Added explanatory note for test agent

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,27 +15,7 @@ module io.sqreen.nashorn {
     provides jdk.dynalink.linker.GuardingDynamicLinkerExporter with
             io.sqreen.nashorn.api.linker.NashornLinkerExporter;
 
-    // TODO: replace exports to .test with jvm parameters when running the tests
-    exports io.sqreen.nashorn.internal.objects to
-            io.sqreen.scripting.nashorn.shell,
-            io.sqreen.nashorn.test;
-
-    exports io.sqreen.nashorn.internal.runtime to
-            io.sqreen.scripting.nashorn.shell,
-            io.sqreen.nashorn.test;
-
-    exports io.sqreen.nashorn.tools to
-            io.sqreen.scripting.nashorn.shell,
-            io.sqreen.nashorn.test;
-
-    opens io.sqreen.nashorn.tools to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.parser to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.runtime.linker to io.sqreen.nashorn.test;
-    opens io.sqreen.nashorn.internal.runtime to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.runtime.options to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.runtime.doubleconv to io.sqreen.nashorn.test;
-    opens io.sqreen.nashorn.internal.runtime.doubleconv to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.runtime.regexp to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.runtime.regexp.joni to io.sqreen.nashorn.test;
-    exports io.sqreen.nashorn.internal.runtime.events to io.sqreen.nashorn.test;
+    exports io.sqreen.nashorn.internal.objects to io.sqreen.scripting.nashorn.shell;
+    exports io.sqreen.nashorn.internal.runtime to io.sqreen.scripting.nashorn.shell;
+    exports io.sqreen.nashorn.tools to io.sqreen.scripting.nashorn.shell;
 }
